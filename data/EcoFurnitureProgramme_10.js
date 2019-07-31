e4369488297dd3daf43a7a7af7a06cf2c5d253ec
planckquark <53330842+planckquark@users.noreply.github.com>
2019-07-31 09:00:21 +0200

    Add files via upload

---
+++ data/EcoFurnitureProgramme_10.js
@@ -0,0 +1,12 @@
+var json_EcoFurnitureProgramme_10 = {
+"type": "FeatureCollection",
+"name": "EcoFurnitureProgramme_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "EcoFurniture" }, "geometry": { "type": "Point", "coordinates": [ 29.995830484000066, -29.360300551999956, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EcoFurniture" }, "geometry": { "type": "Point", "coordinates": [ 30.617867735000061, -29.809007204999943, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EcoFurniture" }, "geometry": { "type": "Point", "coordinates": [ 29.524223212000038, -28.826903519999973, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EcoFurniture" }, "geometry": { "type": "Point", "coordinates": [ 29.964240221000068, -29.669718391999936, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EcoFurniture" }, "geometry": { "type": "Point", "coordinates": [ 30.70276670800007, -29.985109629999954, 0.0 ] } }
+]
+}
